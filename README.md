30dbcd85649cd081728b4d0c75fd16213e8bcc42
Griffin <RGRIFFI5@jaguarlandrover.com>
2017-05-22 11:39:38 +0100

    Neatening and Commenting

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Code for the eChook Nano Telemetry board.
 
 Further documentation here: https://drive.google.com/open?id=12MMUZwj4w5DUC1TnJMnzvs-AOBMiBQhTdfGyMm6rY0o
 
+NOTE: If you are updating your code, ensure you have your calibration.h file backed up - don't overwrite it with our settings!
+
 Dependancies:
 Bounce2 Library - https://github.com/thomasfredericks/Bounce2
     > Handles debouncing of the Launch Mode, Cycle View and Braking Signal
--- eChookCode/Calibration.h
+++ eChookCode/Calibration.h
@@ -1,27 +1,34 @@
 /*
 * This file contains all the values used to calibrate your eChook board.
-* By seperating these it makes it possible to update to newer code without having to copy your
-* calibrations across - just make sure you keep this file!
+* By seperating these it makes it possible to update to newer code without 
+* having to copy your calibrations across - just make sure you keep this file!
 *
-* The values provided by default are for the eChooks team dev board, but each board is different.
-* these will give readings in the right ballpark, but for accuracy, see the documentation to calibrate
-* your own board.
+* The values provided by default are for the eChook's team dev board, but each 
+* board is different. These defaults will give readings in the right ballpark, 
+* but for accuracy, see the documentation to calibrate your own board.
 *
-* Every varialbe in this file begins with CAL_ so that it is identifiable in the main code
+* Every varialbe in this file begins with CAL_ so that it is identifiable in 
+* the main code as being defined in this file.
 *
 */
 
-//Calibration Variables
+//Bluetooth Settings
+const String CAL_BT_NAME     = "eChook"; // Whatever you want to name your car's bluetooth
+const String CAL_BT_PASSWORD = "1234"; // Changing password from default "1234" tends not to work yet - apologies!
 
-const String CAL_BT_NAME = "eChook";
-const String CAL_BT_PASSWORD = "1234";
+// Car Specific Settings
 
+const int       CAL_WHEEL_MAGNETS        = 5; //Number of magnets on wheel
+const int       CAL_MOTOR_MAGNETS        = 6; // Number of magnets on motor shaft for hall effect sensor
+const float     CAL_WHEEL_CIRCUMFERENCE  = 1.178; //Outer circumference of tyre, in Meters. i.e. the distance travelled in one revolution
 
-const float     CAL_REFERENCE_VOLTAGE    = 5;
-const uint8_t   CAL_WHEEL_MAGENTS        = 5;
-const uint8_t   CAL_MOTOR_MAGNETS        = 6; // Number of magnets on motor shaft for hall effect sensor
-const float    CAL_WHEEL_CIRCUMFERENCE  = 1.178; //Outer circumference of tyre, in Meters. i.e. the distance travelled in one revolution
+//Board Specific Calibrations
+const float CAL_REFERENCE_VOLTAGE   = 5;     // Voltage seen on the arduino 5V rail
+const float CAL_BATTERY_TOTAL       = 6.15;  // Multiplier for 24v calculation. Calculated by 24v Input devided by voltage on Arduino pin A0
+const float CAL_BATTERY_LOWER       = 3.071; // Multiplier for 12v calculation. Calculated by 12V Input devided by voltage on Arduino pin A7
+const float CAL_CURRENT             = 37.55; // Current Multiplier - See documentation for calibration method
 
-const float CAL_BATTERY_TOTAL   = 6.15; 
-const float CAL_BATTERY_LOWER   = 3.071;
-const float CAL_CURRENT         = 37.55;
+//Board and Sensor Specific Calibrations
+const float CAL_THERM_A = 0.001871300068; //Steinhart-Hart constants - See documentation for calibration method
+const float CAL_THERM_B = 0.00009436080271;
+const float CAL_THERM_C = 0.0000007954800125;
